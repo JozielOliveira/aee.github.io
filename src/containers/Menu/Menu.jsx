5815e4325ee56203e066bffd6c7a91e4ba5a4b31
JozielOliveira <programdor.joziel@gmail.com>
2019-03-07 18:38:52 -0300

    add description to subtitle pages

--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -6,10 +6,10 @@ import { logout } from '../../store/actions/auth';
 import MenuComponet from '../../components/Menu/Menu.jsx';
 
 const menu = [ 
-    { id: '1', label : 'Agenda', path : 'schedule'}, 
-    { id : '2', label : 'Alunos', path : 'student'}, 
-    { id : '3', label : 'Avaliações', path : 'avaliation'},
-    { id : '4', label : 'CIDs', path : 'cid'}
+    { id: '1', label : 'Agenda', path : 'schedule', description : 'Lista por ordem de agendamento'}, 
+    { id : '2', label : 'Alunos', path : 'student', description : 'Alunos são as pessoas que poderam ser avaliadas no AEE' }, 
+    { id : '3', label : 'Avaliações', path : 'avaliation', description : 'Avaliações são criadas pra aplicar em alunos e obter resultado para analise de CIDs' },
+    { id : '4', label : 'CIDs', path : 'cid', description : 'Classificação Internacional de Doenças' }
 ]
 
 class Menu extends PureComponent {
